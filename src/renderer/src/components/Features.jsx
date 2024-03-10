cb1d9f47e3923f67262947888ebede55eb4dec98
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-03-10 14:00:36 +0800

    Refactor Features component styling

--- src/renderer/src/components/Features.jsx
+++ src/renderer/src/components/Features.jsx
@@ -8,9 +8,11 @@ function Features({ icon, title }) {
       className={`${lineBase} group mx-10 flex cursor-pointer items-center justify-between pb-2 xl:mx-16`}
     >
       <div
-        className={`bg-blue200 flex flex-grow-[0.7] items-center gap-2 rounded-xl px-3 py-2`}
+        className={`group-hover:bg-blue200 flex flex-grow-[0.7] items-center gap-2 rounded-xl bg-transparent transition-colors duration-300 ease-in-out`}
       >
-        <img src={icon} alt="Road Close Icon" className={iconBase} />
+        <div className="bg-blue200 rounded-xl p-2 group-hover:bg-transparent">
+          <img src={icon} alt="Road Close Icon" className={iconBase} />
+        </div>
         <p>{title}</p>
       </div>
       <img
