c412a3e1dbbe8ca26c92c4ff7c41b152358b4d94
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-03-14 18:57:18 +0800

    Update .eslintrc.cjs configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,12 @@
 module.exports = {
   extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
-}
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react/jsx-runtime",
+    "@electron-toolkit",
+    "@electron-toolkit/eslint-config-prettier",
+  ],
+  rules: {
+    "linebreak-style": "off",
+  },
+};
