e5c674a31dfe8374c0354dd3829e94af6e7f1915
devashpontay <devashpontay@gmail.com>
2024-03-16 20:50:21 +0800

    Created custom hook for dijkstra output rendering

--- src/renderer/src/layout/Infobar.jsx
+++ src/renderer/src/layout/Infobar.jsx
@@ -6,37 +6,40 @@ import LocationIcon from "../../../../resources/icons/location.png";
 import DashedIcon from "../../../../resources/icons/dash.png";
 
 import Input from "../components/Input";
+import useDijkstra from "../hooks/useDijkstra";
 
 function Infobar() {
   const [currentLocation, setCurrentLocation] = useState("");
   const [destination, setDestination] = useState("");
-  const [encodedImage, setEncodedImage] = useState('');
-  
+  const [encodedImage, setEncodedImage] = useState("");
+  const setImageData = useDijkstra((state) => state.setSourceImg);
+  const fetchSourceImg = useDijkstra((state) => state.fetchSourceImg);
+
   async function handleSubmit(e) {
     e.preventDefault();
     const start = "A";
     const end = "B";
-    await axios
-      .get(`http://localhost:5000/api/get/shortest/path/${start}/${end}`)
-      .then((response) => {
-        // console.log(response.data);
-        const imageData = response.data.data.encoded_image;
-        setEncodedImage(imageData);
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
-    console.log("dito mo nalang lagay tol");
+    const url = `http://localhost:5000/api/get/shortest/path/${start}/${end}`;
+    fetchSourceImg(url).then((imageData) => {
+      setImageData(`data:image/png;base64,${imageData}`);
+    }).catch((error) => {
+      console.error("Error fetching image:", error);
+    });
   }
+
+
   // handleSubmit()
   return (
     <div className="px-12 py-8">
       <h1 className="text-sm font-bold">Directions</h1>
       <button onClick={handleSubmit}>asda</button>
-            {encodedImage && (
-                <img src={`data:image/png;base64,${encodedImage}`} alt="Encoded Image" />
-            )}
-        </div>
+      {encodedImage && (
+        <img
+          src={`data:image/png;base64,${encodedImage}`}
+          alt="Encoded Image"
+        />
+      )}
+    </div>
     // <div className="px-12 py-8">
     //   {/*directions*/}
     //   <div className="">
--- src/renderer/src/windows/Home.jsx
+++ src/renderer/src/windows/Home.jsx
@@ -1,10 +1,19 @@
+import { useEffect } from "react";
 import mapImage from "../../../../resources/map.png";
 import MapRenderIn3D from "../components/MapRenderIn3D";
+import useDijkstra from "../hooks/useDijkstra";
 
 function Home() {
+  const sourceImg = useDijkstra((state) => state.sourceImg);
+  const setSourceImg = useDijkstra((state) => state.setSourceImg);
+
+  useEffect(() => {
+    setSourceImg(mapImage);
+  }, []);
+
   return (
     <div className="h-screen bg-[#818286]">
-      <MapRenderIn3D imageVal={mapImage} />
+      <MapRenderIn3D imageVal={sourceImg} />
     </div>
   );
 }
