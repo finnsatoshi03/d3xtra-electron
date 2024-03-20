660f14157e9b8ac5d717b5dce956914238742e1e
“jeeybii” <jrrybytjd23@gmail.com>
2024-03-20 23:49:58 +0800

    mazeLoading

---
+++ src/renderer/src/components/loadingMaze.jsx
@@ -0,0 +1,33 @@
+// Loading.jsx
+import React, { useState, useEffect } from 'react';
+
+function Loading() {
+  const [loadingText, setLoadingText] = useState('Loading...');
+
+  useEffect(() => {
+    const texts = [
+      'Loading...',
+      'Plotting Vertices',
+      'Tracing available paths',
+      'Finding obstacles',
+      'Determining best path',
+    ];
+
+    let currentIndex = 0;
+
+    const interval = setInterval(() => {
+      currentIndex = (currentIndex + 1) % texts.length;
+      setLoadingText(texts[currentIndex]);
+    }, 4000); 
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className="loader">
+    <div className="text fade-in">{loadingText}</div>
+  </div>
+  );
+}
+
+export default Loading;
