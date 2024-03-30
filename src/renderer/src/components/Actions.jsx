2b5bd988bf11817443b35354c7291bc6c8a5751a
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-03-30 14:37:14 +0800

    Refactor Actions component to use MapContext

--- src/renderer/src/components/Actions.jsx
+++ src/renderer/src/components/Actions.jsx
@@ -1,25 +1,28 @@
 import { useState } from "react";
+import { useMaps } from "../contexts/MapContext";
 
 function Actions({ icon, title, description, onClick }) {
-  const [isInsertActive, setIsInsertActive] = useState(false);
+  const { isInsertPressed, dispatch } = useMaps();
 
   const handleClick = () => {
     if (title === "Insert") {
-      setIsInsertActive(!isInsertActive);
+      dispatch({ type: "insert/released" });
+    } else {
+      dispatch({ type: "insert/pressed" });
     }
     onClick(title);
   };
 
   return (
     <div
-      className="flex cursor-pointer items-center gap-3 rounded-lg px-2 py-1.5 hover:bg-blue-100 hover:bg-opacity-20"
+      className={`flex cursor-pointer items-center gap-3 rounded-lg px-2 py-1.5 ${title === "Insert" && isInsertPressed && "bg-blue-100 bg-opacity-20"} hover:bg-blue-100 hover:bg-opacity-20`}
       onClick={handleClick}
     >
       <div className="rounded-lg bg-blue200 p-2">
         <img
           src={icon}
           alt="View Icon"
-          className={`size-[15px] transition-transform duration-500 ${title === "Insert" && isInsertActive ? "rotate-45 transform" : ""}`}
+          className={`size-[15px] transition-transform duration-500 ${title === "Insert" && isInsertPressed ? "rotate-45 transform" : ""}`}
         />
       </div>
       <div>
