e88a8b0b763135f6aa19f271f31e4933c5256948
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-04-03 01:21:02 +0800

    Update lighting and add exposure control in Map component

--- src/renderer/src/features/map/Map.jsx
+++ src/renderer/src/features/map/Map.jsx
@@ -1,26 +1,28 @@
-import { Canvas } from "@react-three/fiber";
+/* eslint-disable */
+import { Canvas, useThree } from "@react-three/fiber";
 import { CameraControls, PerspectiveCamera } from "@react-three/drei";
-import { HemisphereLight, DirectionalLight } from "@react-three/drei";
+import { useEffect } from "react";
 
 import Scene from "./Scene";
+
+function Exposure() {
+  const { gl } = useThree();
+
+  useEffect(() => {
+    gl.toneMappingExposure = 0.5;
+  }, [gl]);
+
+  return null;
+}
 
 function Map() {
   return (
     <Canvas shadows={true}>
-      <HemisphereLight
-        skyColor={"#ffffff"}
-        groundColor={"#aaaaaa"}
-        intensity={0.5}
-        position={[0, 50, 0]}
-      />
-      <DirectionalLight
-        color={"#ffffff"}
-        intensity={1}
-        position={[4, 10, -12]}
-        castShadow
-      />
+      <directionalLight position={[-7.6, 10.4, 20]} intensity={5} />
+      <ambientLight intensity={0.5} />
       <PerspectiveCamera makeDefault position={[4, 15, 25]} />
       <Scene />
+      <Exposure />
       <CameraControls />
     </Canvas>
   );
