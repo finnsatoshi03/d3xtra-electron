b543fef1a5c4107ad8e5f96a6bcd0d1fcd853a50
finnsatoshi03 <finnsatoshi03@gmail.com>
2024-04-03 00:48:50 +0800

    Add lighting to the Map component

--- src/renderer/src/features/map/Map.jsx
+++ src/renderer/src/features/map/Map.jsx
@@ -1,14 +1,24 @@
-/* eslint-disable */
 import { Canvas } from "@react-three/fiber";
 import { CameraControls, PerspectiveCamera } from "@react-three/drei";
+import { HemisphereLight, DirectionalLight } from "@react-three/drei";
 
 import Scene from "./Scene";
 
 function Map() {
   return (
     <Canvas shadows={true}>
-      <directionalLight position={[-7.6, 10.4, 20]} intensity={5} />
-      <ambientLight intensity={0.5} />
+      <HemisphereLight
+        skyColor={"#ffffff"}
+        groundColor={"#aaaaaa"}
+        intensity={0.5}
+        position={[0, 50, 0]}
+      />
+      <DirectionalLight
+        color={"#ffffff"}
+        intensity={1}
+        position={[4, 10, -12]}
+        castShadow
+      />
       <PerspectiveCamera makeDefault position={[4, 15, 25]} />
       <Scene />
       <CameraControls />
